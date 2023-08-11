f5e59adbb9a8079cbcc2c96fa66a3bf1f34e6ea4
shahd-alrahbi <138566899+shahd-alrahbi@users.noreply.github.com>
2023-08-11 13:55:19 +0400

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Activity_8_8"
+include(":app")
